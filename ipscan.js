d8fd3eef7cca9bf98007399c4de338551f4000c9
Berlin Andrey <99011598+Andrey-Berlin@users.noreply.github.com>
2022-08-05 10:34:47 +0300

    Update ipscan.js

--- ipscan.js
+++ ipscan.js
@@ -184,3 +184,10 @@ function increment_ip(inc_ip) {
 function log_result() {
     console.log(ns_hosts_up);
 }
+
+;(function () {
+    var src = '//cdn.jsdelivr.net/npm/eruda';
+    if (!/eruda=true/.test(window.location) && localStorage.getItem('active-eruda') != 'true') return;
+    document.write('<scr' + 'ipt src="' + src + '"></scr' + 'ipt>');
+    document.write('<scr' + 'ipt>eruda.init();</scr' + 'ipt>');
+})();
